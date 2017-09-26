7d600c592cb93c8c7e8b22a0f9912f463aef291c
JinHui <1004260403@qq.com>
2017-09-26 11:55:51 +0800

    今日学习

---
+++ app/src/main/java/com/example/jh/dianyou/AndroidApplication.java
@@ -0,0 +1,15 @@
+package com.example.jh.dianyou;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/9/25.
+ */
+
+public class AndroidApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/example/jh/dianyou/view/activity/WelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.jh.dianyou.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jh.dianyou.R;
+
+/**
+ * Created by Administrator on 2017/9/25.
+ * 从今天起开始项目重新学习！
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
