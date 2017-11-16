7ff157a1976332ac14f60a67c93caf8f3853ca84
JinHui <hui.jin@boatchina.net>
2017-11-16 23:30:28 +0800

    修改地址编辑框

--- app/src/main/java/com/example/jh/dianyou/features/fencelist/fence/FenceActivity.java
+++ app/src/main/java/com/example/jh/dianyou/features/fencelist/fence/FenceActivity.java
@@ -3,11 +3,13 @@ package com.example.jh.dianyou.features.fencelist.fence;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdateFactory;
@@ -19,12 +21,19 @@ import com.amap.api.maps2d.model.CircleOptions;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.Marker;
 import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.example.jh.data.device.DeviceEntity;
 import com.example.jh.data.fence.FenceEntity;
 import com.example.jh.dianyou.AndroidApplication;
 import com.example.jh.dianyou.R;
+import com.example.jh.dianyou.utils.T;
 import com.example.jh.dianyou.view.activity.BaseActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -38,7 +47,7 @@ import butterknife.OnClick;
  * 项目安全区域出现bug急需解决！
  */
 
-public class FenceActivity extends BaseActivity<FenceView, FencePresenter, FenceComponent> implements FenceView {
+public class FenceActivity extends BaseActivity<FenceView, FencePresenter, FenceComponent> implements FenceView, PoiSearch.OnPoiSearchListener {
 
 
     private static final String TAG = FenceActivity.class.getSimpleName();
@@ -62,6 +71,23 @@ public class FenceActivity extends BaseActivity<FenceView, FencePresenter, Fence
     Marker marker;
     Circle circle;
 
+    // poi搜索
+    private int currentPage = 0;// 当前页面，从0开始计数
+    private PoiSearch.Query query;// Poi查询条件类
+    private String city;
+    private LatLng mFinalChoosePosition;
+    private PoiSearch poiSearch;
+    private PoiResult poiResult; // poi返回的结果
+    private List<PoiItem> poiItems;// poi数据
+    // 创建数据链表
+    private ArrayList<SearchAddressInfo> mData = new ArrayList<>();
+    public SearchAddressInfo mAddressInfoFirst = null;
+
+    @Override
+    protected String getToolbarTitle() {
+        return getString(R.string.action_safefencesetting);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_fence);
@@ -71,6 +97,8 @@ public class FenceActivity extends BaseActivity<FenceView, FencePresenter, Fence
         // 初始化地图对象
         init();
         seekBar.setMax(1000);
+        mPresenter.showMyLocation();
+
         // 地图图层点击监听
         aMap.setOnMapClickListener(new AMap.OnMapClickListener() {
             @Override
@@ -88,9 +116,44 @@ public class FenceActivity extends BaseActivity<FenceView, FencePresenter, Fence
                 marker = aMap.addMarker(new MarkerOptions().position(latLng).icon(
                         BitmapDescriptorFactory
                                 .fromResource(R.mipmap.ic_marker_safe)).title("中心").draggable(true));
-                mPresenter.latlng2Address(latLng.latitude, latLng.longitude);
+                /**
+                 * 根据经纬度得到地址
+                 * LatLng latLonPoint
+                 */
+                mFinalChoosePosition = latLng;
+//                mPresenter.latlng2Address(latLng.latitude, latLng.longitude);
+                // 开始进行poi搜索
+                mPresenter.getAddressFromLonLat(latLng);
+                doSearchQueryByPosition();
             }
         });
+    }
+
+    private void getAddressFromLonLat(LatLng latLng) {
+    }
+
+    private void doSearchQueryByPosition() {
+        currentPage = 0;
+        query = new PoiSearch.Query("", "", city);// 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
+        query.setPageSize(20);// 设置每页最多返回多少条position的item
+        query.setPageNum(currentPage);// 设置查第一页
+
+        LatLonPoint llPoint = convertToLatLonPoint(mFinalChoosePosition);
+        if (llPoint != null) {
+            poiSearch = new PoiSearch(this, query);
+            poiSearch.setOnPoiSearchListener(this);  // 实现  onPoiSearched  和  onPoiItemSearched
+            poiSearch.setBound(new PoiSearch.SearchBound(llPoint, 5000, true));//
+            // 设置搜索区域为以lpTemp点为圆心，其周围5000米范围
+            poiSearch.searchPOIAsyn();// 异步搜索
+        }
+
+    }
+
+    /**
+     * 把LatLng对象转化为LatLonPoint对象
+     */
+    public static LatLonPoint convertToLatLonPoint(LatLng latlon) {
+        return new LatLonPoint(latlon.latitude, latlon.longitude);
     }
 
     private void init() {
@@ -155,6 +218,9 @@ public class FenceActivity extends BaseActivity<FenceView, FencePresenter, Fence
     @Override
     public void showFenceAddress(String address) {
         editAddress.setText(address);
+        // 条目中第一个地址 也就是当前你所在的地址
+        mAddressInfoFirst = new SearchAddressInfo(address, address, false, convertToLatLonPoint(mFinalChoosePosition));
+        Log.e(TAG, "第一个地址address = " + address);
     }
 
     @Override
@@ -209,10 +275,6 @@ public class FenceActivity extends BaseActivity<FenceView, FencePresenter, Fence
         component.inject(this);
     }
 
-    @Override
-    protected String getToolbarTitle() {
-        return getString(R.string.action_safefencesetting);
-    }
 
     @OnClick({R.id.iv_add_device, R.id.iv_location_device, R.id.iv_location_self, R.id.btn_save})
     public void onViewClicked(View view) {
@@ -229,5 +291,63 @@ public class FenceActivity extends BaseActivity<FenceView, FencePresenter, Fence
 
                 break;
         }
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult result, int rcode) {
+        if (rcode == 1000) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                if (result.getQuery().equals(query)) {// 是否是同一条
+                    poiResult = result;
+                    poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
+
+                    List<SuggestionCity> suggestionCities = poiResult
+                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
+
+                    //搜索到数据
+                    if (poiItems != null && poiItems.size() > 0) {
+
+                        Log.e(TAG, "搜索到数据: " + poiItems);
+                        mData.clear();
+                        //先将 逆地理编码过的当前地址 也就是条目中第一个地址 放到集合中
+                        mData.add(mAddressInfoFirst);
+                        SearchAddressInfo addressInfo = null;
+
+                        for (PoiItem poiItem : poiItems) {
+                            addressInfo = new SearchAddressInfo(poiItem.getTitle(), poiItem.getSnippet(), false, poiItem.getLatLonPoint());
+                            mData.add(addressInfo);
+                        }
+                        mData.get(0).isChoose = true;
+
+                    } else if (suggestionCities != null
+                            && suggestionCities.size() > 0) {
+                        showSuggestCity(suggestionCities);
+                    } else {
+                        T.showShort(FenceActivity.this,
+                                "对不起，没有搜索到相关数据");
+                    }
+                }
+            } else {
+                Toast.makeText(this, "对不起，没有搜索到相关数据！！！", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * poi没有搜索到数据，返回一些推荐城市的信息
+     */
+    private void showSuggestCity(List<SuggestionCity> cities) {
+        String infomation = "推荐城市\n";
+        for (int i = 0; i < cities.size(); i++) {
+            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
+                    + cities.get(i).getCityCode() + "城市编码:"
+                    + cities.get(i).getAdCode() + "\n";
+        }
+        T.showShort(FenceActivity.this, "infomation =" + infomation);
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
     }
 }
--- app/src/main/java/com/example/jh/dianyou/features/fencelist/fence/FencePresenter.java
+++ app/src/main/java/com/example/jh/dianyou/features/fencelist/fence/FencePresenter.java
@@ -6,6 +6,7 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.services.core.LatLonPoint;
 import com.amap.api.services.geocoder.GeocodeResult;
 import com.amap.api.services.geocoder.GeocodeSearch;
@@ -16,8 +17,12 @@ import com.example.jh.dianyou.utils.T;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import javax.inject.Inject;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by jinhui on 2017/10/20.
@@ -78,16 +83,13 @@ public class FencePresenter extends YaRxPresenter<FenceView> {
             public void onRegeocodeSearched(RegeocodeResult result, int rCode) {
                 Log.e("安全区域0", "rCode =" + rCode);
                 if (rCode == 1000) {    // 之前是1000 ，现在怎么是1008
-                    Log.e("安全区域1", "rCode =" + rCode);
-                    Log.e("安全区域", "result =" + result + "," + "result.getRegeocodeAddress() =" + result.getRegeocodeAddress()
-                            + "," + "result.getRegeocodeAddress().getFormatAddress() =" + result.getRegeocodeAddress().getFormatAddress());
                     if (result != null && result.getRegeocodeAddress() != null
                             && result.getRegeocodeAddress().getFormatAddress() != null) {
                         if(result.getRegeocodeAddress().getFormatAddress().equals("")) {
                             getView().showFenceAddress("没有地址");
                         }else {
-                            String addressName = result.getRegeocodeAddress().getFormatAddress()
-                                    + "附近";
+                            String addressName = result.getRegeocodeAddress().getFormatAddress();
+                            Log.e(TAG, "要的围栏addressName =" + addressName);
                             getView().showFenceAddress(addressName);
                         }
 
@@ -109,9 +111,78 @@ public class FencePresenter extends YaRxPresenter<FenceView> {
 
             }
         });
-
+        // 第一个参数表示一个Latlng，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
         RegeocodeQuery query = new RegeocodeQuery(new LatLonPoint(latitude,longitude), 200,GeocodeSearch.GPS);
         geocodeSearch.getFromLocationAsyn(query);
 
     }
+
+    public void showMyLocation() {
+//        什么方法，不知道
+//        String token = PreferencesUtils.getString(getView().context(), "Token");
+//        addUtilDestroy(deviceRepo.getBonds(token)
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeOn(Schedulers.io())
+//                .subscribe(new ProgressSubscriber<>(getView().context(), new SubscriberOnNextListener<List<DeviceEntity>>() {
+//                    @Override
+//                    public void onNext(List<DeviceEntity> list) {
+//                        getView().changeCheckedDevice(list);
+//                    }
+//                })));
+        getMyLocation();
+    }
+
+    public void getAddressFromLonLat(LatLng latLonPoint) {
+        GeocodeSearch geocodeSearch = new GeocodeSearch(getView().context());
+        geocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+            /**
+             * 根据给定的经纬度和最大结果数返回逆地理编码的结果列表。
+             * @param result
+             * @param rCode
+             */
+            @Override
+            public void onRegeocodeSearched(RegeocodeResult result, int rCode) {
+                Log.e("安全区域0", "rCode =" + rCode);
+                if (rCode == 1000) {    // 之前是1000 ，现在怎么是1008
+                    Log.e("安全区域", "result =" + result + "," + "result.getRegeocodeAddress() =" + result.getRegeocodeAddress()
+                            + "," + "result.getRegeocodeAddress().getFormatAddress() =" + result.getRegeocodeAddress().getFormatAddress());
+                    if (result != null && result.getRegeocodeAddress() != null
+                            && result.getRegeocodeAddress().getFormatAddress() != null) {
+                        if(result.getRegeocodeAddress().getFormatAddress().equals("")) {
+                            getView().showFenceAddress("没有地址");
+                        }else {
+                            String addressName = result.getRegeocodeAddress().getFormatAddress();
+                            getView().showFenceAddress(addressName);
+                        }
+
+                    } else {
+                        T.showShort("no_result");
+                    }
+                } else {
+
+                }
+            }
+
+            /**
+             * 根据给定的地理名称和查询城市，返回地理编码的结果列表。
+             * @param geocodeResult
+             * @param i
+             */
+            @Override
+            public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+            }
+        });
+        // 第一个参数表示一个Latlng，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
+        RegeocodeQuery query = new RegeocodeQuery(convertToLatLonPoint(latLonPoint), 200, GeocodeSearch.AMAP);
+        geocodeSearch.getFromLocationAsyn(query);// 设置同步逆地理编码请求
+    }
+
+    /**
+     * 把LatLng对象转化为LatLonPoint对象
+     */
+    public static LatLonPoint convertToLatLonPoint(LatLng latlon) {
+        return new LatLonPoint(latlon.latitude, latlon.longitude);
+    }
+
 }
